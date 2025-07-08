73489bb5488d3415d79e9827114b374e9e774a7e
Infonioknight <gaurav3594@gmail.com>
2025-07-09 02:10:01 +0530

    removed secret

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const databaseConnection = () => {
-    mongoose.connect("mongodb+srv://gpremp7736:GameGame@cluster0.y56rpua.mongodb.net/Real-problem-on-Size", {
-    // mongoose.connect("mongodb+srv://gpremp7736:GameGame@cluster0.y56rpua.mongodb.net/test-Real-problem-on-Size", {
+    mongoose.connect(process.env.MONGO_URI, {
         autoIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     })
         .then(() => {
             console.log('MongoDB is connected on 27017')
